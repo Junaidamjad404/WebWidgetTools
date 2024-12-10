9a483b18953eccfa418c59dfcbc8cc9268271ac8
Junaid Amjad <junaid.amjad@tetralogicx.com>
2024-12-10 19:33:23 +0500

    seeder Description changes

--- web/database/seeders/GeneralModulesTableSeeder.php
+++ web/database/seeders/GeneralModulesTableSeeder.php
@@ -31,7 +31,7 @@ class GeneralModulesTableSeeder extends Seeder
             [
                 'name' => 'First Sign Up Discount',
                 'handle'=> 'First_Sign_Up_Discount',
-                'description' => 'A customizable header module for branding and navigation.',
+                'description' => 'A special discount offered to customers when they sign up for the first time.',
                 'image'=> 'images/general_modules/FSD.png',
                 'settings' => json_encode($metafieldValue),
                 'created_at' => now(),
@@ -40,7 +40,7 @@ class GeneralModulesTableSeeder extends Seeder
             [
                 'name' => 'Estimated Shipping Date and time',
                 'handle' => 'Estimated_Shipping_Date_and_time',
-                'description' => 'A customizable footer module for contact information and links.',
+                'description' => 'The expected date and time when the order will be shipped to the customer.',
                 'image'=> 'images/general_modules/images.png',
                 'settings' => json_encode($metafieldValue),
                 'created_at' => now(),
@@ -49,7 +49,7 @@ class GeneralModulesTableSeeder extends Seeder
             [
                 'name' => 'Shipping Details',
                 'handle' => 'Shipping_Details',
-                'description' => 'A customizable sidebar module for showcasing categories and offers.',
+                'description' => 'Information about the delivery address, shipping method, and related logistics for an order.',
                 'image' => 'images/general_modules/shipping_details.png',
                 'settings' => json_encode($metafieldValue),
                 'created_at' => now(),
